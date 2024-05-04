202aaa21e91c72832c662eaa04907e325b2a0d36
Jeffrey Vavasour <jeffreyvavasour@gmail.com>
2024-05-04 22:28:42 +0100

    initial commit

---
+++ test/models/article_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class ArticleTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "should not save an article without a title" do
+    article = Article.new
+    article.body = "article body"
+    assert_not project.save, "Saved the project without a title"
+  end
+
+  test "should not save an article without a body" do
+    article = Article.new
+    article.title = "article title"
+    assert_not project.save, "Saved the project without a body"
+  end
+end
