4708a8a58ac11e87e2a0896a8a74ab205cc69b56
Jeffrey Vavasour <jeffreyvavasour@gmail.com>
2024-05-05 16:22:41 +0100

    added test run to config.yml

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -8,12 +8,12 @@ class ArticleTest < ActiveSupport::TestCase
   test "should not save an article without a title" do
     article = Article.new
     article.body = "article body"
-    assert_not project.save, "Saved the project without a title"
+    assert_not article.save, "Saved the project without a title"
   end
 
   test "should not save an article without a body" do
     article = Article.new
     article.title = "article title"
-    assert_not project.save, "Saved the project without a body"
+    assert_not article.save, "Saved the project without a body"
   end
 end
